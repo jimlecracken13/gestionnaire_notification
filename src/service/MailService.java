33df05e6b26e1f72123fafcc3c4afd90dc60a96b
jimlecracken13 <jimlecracken13@gmail.com>
2025-06-17 20:57:15 +0000

    configurations

--- src/service/MailService.java
+++ src/service/MailService.java
@@ -4,22 +4,39 @@ import jakarta.mail.*;
 import jakarta.mail.internet.*;
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 import java.util.Properties;
 
 public class MailService {
-
+    //on charge les properties
+    Properties config = new Properties();
+    InputStream input = MailService.class.getClassLoader().getResourceAsStream("resources.properties");
+    public void configLoader()
+    {
+        try {
+            config.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     // L’email de l'application
     private final String expediteur = "notifplusapp@gmail.com";
     private final String motDePasse = "txjkmukvfznvxkvo";
 
     public void envoyerEmail(String nomExpediteur, String destinataire, String sujet, String contenu) throws UnsupportedEncodingException, MessagingException {
         Properties props = new Properties();
+       /*
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
+        props.put("mail.smtp.port", "587");*/
+        props.put("mail.smtp.auth",config.getProperty("mail.smtp.auth"));
+        props.put("mail.smtp.starttls.enable", config.getProperty("mail.smtp.starttls.enable"));
+        props.put("mail.smtp.host", config.getProperty("mail.smtp.host"));
+        props.put("mail.smtp.port", config.getProperty("mail.smtp.port"));
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(expediteur, motDePasse);
